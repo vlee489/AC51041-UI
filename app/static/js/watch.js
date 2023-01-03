6064c8a667a82e9c7ed851600919bc1e4b8812bb
Vincent Lee <vlee@vlee.me.uk>
2023-01-03 04:33:08 +0000

    save user's view history and play back location

--- app/static/js/watch.js
+++ app/static/js/watch.js
@@ -1,5 +1,8 @@
 let player = videojs('my-video', {aspectRatio: "16:9"});
 let continue_alert = new bootstrap.Alert(document.getElementById('continue_film'))
+let continue_time = 0;
+let tags = [];
+let categories = [];
 
 async function loadInfo(){
     const response = await fetch(`https://devops.vlee.me.uk/film/id/${getURLPrams()['fim_id']}`, {
@@ -15,8 +18,25 @@ async function loadInfo(){
         document.getElementById("description").innerHTML = response.description;
         document.getElementById("tags").innerHTML = `Tags: ${response.tags}`;
         document.getElementById("categories").innerHTML = `Category: ${response.categories}`;
+        tags = response.tags;
+        categories = response.categories;
         
     }
+}
+
+
+async function loadContinue(){
+    const response = await fetch(`https://devops.vlee.me.uk/watch/${getURLPrams()['fim_id']}`, {
+        headers: {"content-type": "application/json", "Authorization": getCookies().session},
+        method: 'get',
+    }).then(function(response) {
+        if (!response.ok) {
+            continue_alert.close()
+        }else{
+            response => response.json()
+            continue_time = response.pos;
+        }
+    })
 }
 
 async function loadFilm(){
@@ -36,19 +56,41 @@ async function loadFilm(){
 (async () => {
     await loadInfo();
     await loadFilm();
+    await loadContinue();
 })()
 
 
 player.on('play', function() {
-    continue_alert.close()
+    if (continue_time > 0){
+        continue_alert.close()
+    }
+    fetch(`https://devops.vlee.me.uk/rec/`, {
+        headers: {"content-type": "application/json", "Authorization": getCookies().session},
+        method: 'POST',
+        body: JSON.stringify({"tags": tags, "categories": categories})
+    })
 });
 
 player.on('pause', function() {
     console.log('paused')
+    fetch(`https://devops.vlee.me.uk/watch/${getURLPrams()['fim_id']}`, {
+        headers: {"content-type": "application/json", "Authorization": getCookies().session},
+        method: 'POST',
+        body: JSON.stringify({pos: player.currentTime()})
+    })
+});
+
+player.on('ended', function() {
+    console.log('finished')
+    fetch(`https://devops.vlee.me.uk/watch/${getURLPrams()['fim_id']}`, {
+        headers: {"content-type": "application/json", "Authorization": getCookies().session},
+        method: 'DELETE',
+        body: JSON.stringify({pos: player.currentTime()})
+    })
 });
 
 function resume(){
-    player.currentTime(10)
+    player.currentTime(continue_time)
     player.play()
     continue_alert.close()
 }
